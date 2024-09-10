957f058c9d3f773c07ebd105e33db6b07f0508f4
B-Milda <borek.milda@gmail.com>
2024-09-10 08:18:35 +0200

    Add files via upload
    Upravená, finální verze - nyní funkční výpočet pro políčka s vícero vzdálenostmi

--- script.js
+++ script.js
@@ -1,33 +1,73 @@
-let map = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-  [0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-  [0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
-  [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
-  [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0],
-  [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0],
-  [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-  [0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-  [0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
-  [0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-  [0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-  [0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-  [0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
+/* Maximální vzdálenost pro výpočet: */
+const maxRange = 20;
 
+let map = [
+    ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
+    ['W', 'R', 'R', 'R', 'W', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'W'],
+    ['W', 'R', 'W', 'R', 'W', 'R', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'R', 'W'],
+    ['W', 'R', 'W', 'R', 'W', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'W', 'R', 'W'],
+    ['W', 'R', 'W', 'R', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'R', 'W', 'R', 'W'],
+    ['W', 'R', 'W', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'W', 'R', 'W', 'R', 'W'],
+    ['W', 'R', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'R', 'W', 'R', 'W', 'R', 'W'],
+    ['W', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'W', 'R', 'R', 'R', 'W', 'R', 'W'],
+    ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'R', 'W', 'R', 'W', 'R', 'W', 'R', 'W'],
+    ['W', 'R', 'R', 'R', 'R', 'R', 'R', 'W', 'R', 'W', 'R', 'W', 'R', 'W', 'R', 'W'],
+    ['W', 'R', 'W', 'W', 'W', 'W', 'R', 'R', 'R', 'W', 'R', 'W', 'R', 'W', 'R', 'W'],
+    ['W', 'R', 'W', 'R', 'R', 'W', 'R', 'W', 'R', 'W', 'R', 'W', 'R', 'W', 'R', 'W'],
+    ['W', 'R', 'W', 'R', 'W', 'W', 'R', 'W', 'R', 'W', 'R', 'W', 'R', 'W', 'R', 'W'],
+    ['W', 'R', 'W', 'R', 'R', 'R', 'R', 'W', 'R', 'W', 'R', 'W', 'R', 'W', 'R', 'W'],
+    ['W', 'R', 'R', 'R', 'W', 'W', 'W', 'W', 'R', 'R', 'R', 'W', 'R', 'R', 'R', 'W'],
+    ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W'],
+  ];
+
+/* W = Wall, R = Road */  
+
+/* Vytvoření kopie mapy, parse & stringify pro zrušení reference */
 const mapOri = JSON.parse(JSON.stringify(map));
 
-function rangeFinder(x, y, range = 2) {
-  if (map[x][y] != 0 && map[x][y] < 2 && range <= 20) {
-    map[x][y] = range;
-    rangeFinder(x + 1, y, range + 1);
-    rangeFinder(x - 1, y, range + 1);
-    rangeFinder(x, y + 1, range + 1);
-    rangeFinder(x, y - 1, range + 1);
+function rangeFinder(x, y, range = 0, sourceRoute){
+    rangeFinderBase(x, y, range, sourceRoute);
+    domHandler();
+}
+
+function rangeFinderBase(x, y, range = 0, sourceRoute) {
+  if (map[x][y] != "W" && range <= maxRange) {
+    /* Pokud je původní cesta R, nahradí R za range, jinak přidá hodnotu ke stávající*/
+    if (map[x][y] === "R") {
+      map[x][y] = range;
+    } else {
+      map[x][y] = map[x][y] + "," + range;
+    }
+    /* Switch pro rekurzní opakování, aby se funkce nevracela odkud přišla */ 
+    switch (sourceRoute) {
+      case "x+":
+        rangeFinderBase(x + 1, y, range + 1, "x+");
+        rangeFinderBase(x, y + 1, range + 1, "y+");
+        rangeFinderBase(x, y - 1, range + 1, "y-");
+        break;
+      case "x-":
+        rangeFinderBase(x - 1, y, range + 1, "x-");
+        rangeFinderBase(x, y + 1, range + 1, "y+");
+        rangeFinderBase(x, y - 1, range + 1, "y-");
+        break;
+      case "y+":
+        rangeFinderBase(x + 1, y, range + 1, "x+");
+        rangeFinderBase(x - 1, y, range + 1, "x-");
+        rangeFinderBase(x, y + 1, range + 1, "y+");
+        break;
+      case "y-":
+        rangeFinderBase(x + 1, y, range + 1, "x+");
+        rangeFinderBase(x - 1, y, range + 1, "x-");
+        rangeFinderBase(x, y - 1, range + 1, "y-");
+        break;
+      default:
+        rangeFinderBase(x + 1, y, range + 1, "x+");
+        rangeFinderBase(x - 1, y, range + 1, "x-");
+        rangeFinderBase(x, y + 1, range + 1, "y+");
+        rangeFinderBase(x, y - 1, range + 1, "y-");
+        break;
+    }
   }
-  domHandler();
 }
 
 function domHandler() {
@@ -35,17 +75,24 @@ function domHandler() {
   for (let i = 0; i < map.length; i++) {
     for (let u = 0; u < map[i].length; u++) {
       const elem = document.createElement("div");
-      if (map[i][u] === 0) {
+      /* Vykreslení zdi */
+      if (map[i][u] === "W") {
         elem.classList.add("wall");
-      } else if (map[i][u] === 1) {
+        /* Vykreslení cesty */
+      } else if (map[i][u] === "R") {
         elem.addEventListener("click", function () {
+          reset();
           rangeFinder(i, u);
         });
-      } else if (map[i][u] === 2) {
+        /* Vykreslení vybraného políčka*/        
+        // Podmínka pokud počáteční políčko je zároveň vzdálenostní (začíná 0)
+      } else if (map[i][u] === 0 || (typeof map[i][u] === "string" && map[i][u].startsWith("0"))) {
         elem.classList.add("selected");
+        /* Vykreslení vzdálenosti */
       } else {
-        elem.innerText = map[i][u] - 2;
+        elem.innerText = map[i][u];
         elem.addEventListener("click", function () {
+          reset();
           rangeFinder(i, u);
         });
       }
@@ -64,4 +111,4 @@ function reset() {
 
 /* Podmínka pro RangeFinder, kdyby mapa nebyla uzavřená:
     map[x]!== undefined && map[x][y]!==undefined 
-    */
+*/
